46e3e1d0748c32f00834d64244c0203d87f013d7
manierod <davide.maniero@skytv.it>
2019-09-22 01:12:40 +0200

    Fixed product tests

--- src/test/java/it/exercise/salestaxes/model/Product/ProductTest.java
+++ src/test/java/it/exercise/salestaxes/model/Product/ProductTest.java
@@ -3,11 +3,13 @@ package it.exercise.salestaxes.model.Product;
 import it.exercise.salestaxes.model.BaseTest;
 import it.exercise.salestaxes.model.Product.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 public class ProductTest extends BaseTest {
 
+    @Test
     public void shouldCreateProductWithNameAndPrice(){
         Product product = new Product(productName,price);
 
@@ -16,6 +18,7 @@ public class ProductTest extends BaseTest {
         Assertions.assertEquals(price, product.getOriginalPrice());
     }
 
+    @Test
     public void shouldCreateProductWithoutTaxesAndWithTotalPriceEqualToOriginalPrices(){
         Product product = new Product(productName,price);
 
@@ -23,6 +26,7 @@ public class ProductTest extends BaseTest {
         Assertions.assertEquals(price, product.getOriginalPrice());
     }
 
+    @Test
     public void shouldThrowNullPointerExceptionWhenProductNameOrPriceAreNull(){
         Assertions.assertThrows(NullPointerException.class, () -> new Product(productName,null));
         Assertions.assertThrows(NullPointerException.class, () -> new Product(null, price));
